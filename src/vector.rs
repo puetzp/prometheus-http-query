29c5452a60886c22853a3daabbc971ef99f7a366
puetzp <peter.puetz@itenos.de>
2021-06-15 15:20:12 +0200

    Implement arith. scalar operations for InstantVector

--- src/vector.rs
+++ src/vector.rs
@@ -153,6 +153,290 @@ impl InstantVector {
 
         InstantVector(this)
     }
+
+    /// Multiply one instant vector by another. Additional modifiers (`Match` and `Group`)
+    /// can be used to alter the matching behaviour between two instant vectors.
+    /// See the [Prometheus reference](https://prometheus.io/docs/prometheus/latest/querying/operators/#arithmetic-binary-operators)
+    /// for details on this topic.
+    ///
+    /// ```rust
+    /// use prometheus_http_query::Selector;
+    /// use prometheus_http_query::InstantVector;
+    /// use prometheus_http_query::Match;
+    /// use std::convert::TryInto;
+    ///
+    /// let one: InstantVector = Selector::new()
+    ///     .metric("some_metric")
+    ///     .unwrap()
+    ///     .with("some_label", "some_value")
+    ///     .try_into()
+    ///     .unwrap();
+    ///
+    /// let two: InstantVector = Selector::new()
+    ///     .metric("other_metric")
+    ///     .unwrap()
+    ///     .with("some_label", "some_value")
+    ///     .with("other_label", "other_value")
+    ///     .try_into()
+    ///     .unwrap();
+    ///
+    /// let new = one.multiply(two, Some(Match::On(&["some_label"])), None);
+    ///
+    /// let expected = String::from("some_metric{some_label=\"some_value\"} * on (some_label) other_metric{some_label=\"some_value\",other_label=\"other_value\"}");
+    ///
+    /// assert_eq!(new.to_string(), expected);
+    /// ```
+    pub fn multiply(
+        self,
+        other: InstantVector,
+        match_modifier: Option<Match>,
+        group_modifier: Option<Group>,
+    ) -> InstantVector {
+        let InstantVector(mut this) = self;
+        let InstantVector(other) = other;
+
+        this.push_str(" *");
+
+        if let Some(labels) = match_modifier {
+            this.push_str(&format!(" {}", labels.to_string()));
+        }
+
+        if let Some(labels) = group_modifier {
+            this.push_str(&format!(" {}", labels.to_string()));
+        }
+
+        this.push_str(&format!(" {}", other));
+
+        InstantVector(this)
+    }
+
+    /// Divide one instant vector by another. Additional modifiers (`Match` and `Group`)
+    /// can be used to alter the matching behaviour between two instant vectors.
+    /// See the [Prometheus reference](https://prometheus.io/docs/prometheus/latest/querying/operators/#arithmetic-binary-operators)
+    /// for details on this topic.
+    ///
+    /// ```rust
+    /// use prometheus_http_query::Selector;
+    /// use prometheus_http_query::InstantVector;
+    /// use prometheus_http_query::Match;
+    /// use std::convert::TryInto;
+    ///
+    /// let one: InstantVector = Selector::new()
+    ///     .metric("some_metric")
+    ///     .unwrap()
+    ///     .with("some_label", "some_value")
+    ///     .try_into()
+    ///     .unwrap();
+    ///
+    /// let two: InstantVector = Selector::new()
+    ///     .metric("other_metric")
+    ///     .unwrap()
+    ///     .with("some_label", "some_value")
+    ///     .with("other_label", "other_value")
+    ///     .try_into()
+    ///     .unwrap();
+    ///
+    /// let new = one.divide(two, Some(Match::Ignoring(&["other_label"])), None);
+    ///
+    /// let expected = String::from("some_metric{some_label=\"some_value\"} / ignoring (other_label) other_metric{some_label=\"some_value\",other_label=\"other_value\"}");
+    ///
+    /// assert_eq!(new.to_string(), expected);
+    /// ```
+    pub fn divide(
+        self,
+        other: InstantVector,
+        match_modifier: Option<Match>,
+        group_modifier: Option<Group>,
+    ) -> InstantVector {
+        let InstantVector(mut this) = self;
+        let InstantVector(other) = other;
+
+        this.push_str(" /");
+
+        if let Some(labels) = match_modifier {
+            this.push_str(&format!(" {}", labels.to_string()));
+        }
+
+        if let Some(labels) = group_modifier {
+            this.push_str(&format!(" {}", labels.to_string()));
+        }
+
+        this.push_str(&format!(" {}", other));
+
+        InstantVector(this)
+    }
+
+    /// Divide one instant vector by another with remainder. Additional modifiers (`Match` and `Group`)
+    /// can be used to alter the matching behaviour between two instant vectors.
+    /// See the [Prometheus reference](https://prometheus.io/docs/prometheus/latest/querying/operators/#arithmetic-binary-operators)
+    /// for details on this topic.
+    ///
+    /// ```rust
+    /// use prometheus_http_query::Selector;
+    /// use prometheus_http_query::InstantVector;
+    /// use prometheus_http_query::Match;
+    /// use std::convert::TryInto;
+    ///
+    /// let one: InstantVector = Selector::new()
+    ///     .metric("some_metric")
+    ///     .unwrap()
+    ///     .with("some_label", "some_value")
+    ///     .try_into()
+    ///     .unwrap();
+    ///
+    /// let two: InstantVector = Selector::new()
+    ///     .metric("other_metric")
+    ///     .unwrap()
+    ///     .with("some_label", "some_value")
+    ///     .with("other_label", "other_value")
+    ///     .try_into()
+    ///     .unwrap();
+    ///
+    /// let new = one.modulo(two, Some(Match::Ignoring(&["other_label"])), None);
+    ///
+    /// let expected = String::from("some_metric{some_label=\"some_value\"} % ignoring (other_label) other_metric{some_label=\"some_value\",other_label=\"other_value\"}");
+    ///
+    /// assert_eq!(new.to_string(), expected);
+    /// ```
+    pub fn modulo(
+        self,
+        other: InstantVector,
+        match_modifier: Option<Match>,
+        group_modifier: Option<Group>,
+    ) -> InstantVector {
+        let InstantVector(mut this) = self;
+        let InstantVector(other) = other;
+
+        this.push_str(" %");
+
+        if let Some(labels) = match_modifier {
+            this.push_str(&format!(" {}", labels.to_string()));
+        }
+
+        if let Some(labels) = group_modifier {
+            this.push_str(&format!(" {}", labels.to_string()));
+        }
+
+        this.push_str(&format!(" {}", other));
+
+        InstantVector(this)
+    }
+
+    /// Exponentiate one instant vector by another. Additional modifiers (`Match` and `Group`)
+    /// can be used to alter the matching behaviour between two instant vectors.
+    /// See the [Prometheus reference](https://prometheus.io/docs/prometheus/latest/querying/operators/#arithmetic-binary-operators)
+    /// for details on this topic.
+    ///
+    /// ```rust
+    /// use prometheus_http_query::Selector;
+    /// use prometheus_http_query::InstantVector;
+    /// use prometheus_http_query::Match;
+    /// use std::convert::TryInto;
+    ///
+    /// let one: InstantVector = Selector::new()
+    ///     .metric("some_metric")
+    ///     .unwrap()
+    ///     .with("some_label", "some_value")
+    ///     .try_into()
+    ///     .unwrap();
+    ///
+    /// let two: InstantVector = Selector::new()
+    ///     .metric("other_metric")
+    ///     .unwrap()
+    ///     .with("some_label", "some_value")
+    ///     .with("other_label", "other_value")
+    ///     .try_into()
+    ///     .unwrap();
+    ///
+    /// let new = one.power(two, Some(Match::Ignoring(&["other_label"])), None);
+    ///
+    /// let expected = String::from("some_metric{some_label=\"some_value\"} ^ ignoring (other_label) other_metric{some_label=\"some_value\",other_label=\"other_value\"}");
+    ///
+    /// assert_eq!(new.to_string(), expected);
+    /// ```
+    pub fn power(
+        self,
+        other: InstantVector,
+        match_modifier: Option<Match>,
+        group_modifier: Option<Group>,
+    ) -> InstantVector {
+        let InstantVector(mut this) = self;
+        let InstantVector(other) = other;
+
+        this.push_str(" ^");
+
+        if let Some(labels) = match_modifier {
+            this.push_str(&format!(" {}", labels.to_string()));
+        }
+
+        if let Some(labels) = group_modifier {
+            this.push_str(&format!(" {}", labels.to_string()));
+        }
+
+        this.push_str(&format!(" {}", other));
+
+        InstantVector(this)
+    }
+}
+
+impl std::ops::Add<f64> for InstantVector {
+    type Output = Self;
+
+    fn add(self, scalar: f64) -> Self {
+        let InstantVector(mut vec) = self;
+        vec.push_str(&format!(" + {}", scalar.to_string()));
+        InstantVector(vec)
+    }
+}
+
+impl std::ops::Sub<f64> for InstantVector {
+    type Output = Self;
+
+    fn sub(self, scalar: f64) -> Self {
+        let InstantVector(mut vec) = self;
+        vec.push_str(&format!(" - {}", scalar.to_string()));
+        InstantVector(vec)
+    }
+}
+
+impl std::ops::Mul<f64> for InstantVector {
+    type Output = Self;
+
+    fn mul(self, scalar: f64) -> Self {
+        let InstantVector(mut vec) = self;
+        vec.push_str(&format!(" * {}", scalar.to_string()));
+        InstantVector(vec)
+    }
+}
+
+impl std::ops::Div<f64> for InstantVector {
+    type Output = Self;
+
+    fn div(self, scalar: f64) -> Self {
+        let InstantVector(mut vec) = self;
+        vec.push_str(&format!(" / {}", scalar.to_string()));
+        InstantVector(vec)
+    }
+}
+
+impl std::ops::Rem<f64> for InstantVector {
+    type Output = Self;
+
+    fn rem(self, scalar: f64) -> Self {
+        let InstantVector(mut vec) = self;
+        vec.push_str(&format!(" % {}", scalar.to_string()));
+        InstantVector(vec)
+    }
+}
+
+impl std::ops::BitXor<f64> for InstantVector {
+    type Output = Self;
+
+    fn bitxor(self, scalar: f64) -> Self {
+        let InstantVector(mut vec) = self;
+        vec.push_str(&format!(" ^ {}", scalar.to_string()));
+        InstantVector(vec)
+    }
 }
 
 #[derive(Debug, PartialEq)]
